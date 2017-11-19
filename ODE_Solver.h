2b794b1da37983484253f6758d660e9457b506bf
SagarShetye <33704428+SagarShetye@users.noreply.github.com>
2017-11-19 12:51:25 -0500

    ODE_Solver.h
    Modified solveVelocity() and solveDisp() functions

--- ODE_Solver.h
+++ ODE_Solver.h
@@ -27,10 +27,10 @@ public:
 	void spring_init(const Spring&,const Spring&,const Spring&);						// Takes Spring objects from the main() and fills springStiffness[]
 	void damper_init(const Damper&, const Damper&, const Damper&);						// Takes an Damper objects from the main() and fills dampCoefficient[]
 	void force_init(const Force&, const Force&);							// Initializes the forcing functions; Takes Force objects from the main()
-	double solveDisp(double);									// Solves the ODE system for displacement; Takes timeStep from the main()
+	void solveDisp(double, Mass&, Mass&);									// Solves the ODE system for displacement; Takes timeStep from the main()
 																// It internally takes care of whether it's a 2 mass system or single mass system
 																// User only needs to call this function from the main() and the displacement at corresponding time step will be calculated
-	double solveVelocity(double);								// Solves the ODE system for the velocity
+	void solveVelocity(double, Mass&, Mass&);								// Solves the ODE system for the velocity
 																// User only needs to call this function from the main() and the velocity at corresponding time step will be calculated
 	void reset();												// Reset the variables when user presses the reset button
 	void CleanUp();												// To release memory
