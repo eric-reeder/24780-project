f52f8dfe8d7398552d6313dac76211b897bac458
SagarShetye <33704428+SagarShetye@users.noreply.github.com>
2017-11-20 15:36:24 -0500

    Added functionality to Force.cpp

--- Force.cpp
+++ Force.cpp
@@ -1,7 +1,38 @@
 #include "Force.h"
+#include<assert.h>
 
-
+// Constructor
 Force::Force()
 {
-	// Code here
+	type = -1;
+}
+
+void Force::setType(int force_type)
+{
+	type = force_type;
+}
+
+int Force::getType() const
+{
+	return type;
+}
+
+void Force::setValue(double val1, double val2 = 0)
+{
+	assert(type != -1);										// To ensure the type has been initialized
+															// Program will stop running and point to this error
+	if (type == 2)
+	{
+		value.push_back(val1);								// Constant function
+	}
+	else
+	{
+		value.push_back(val1);								// Sine or exponential function
+		value.push_back(val2);
+	}
+}
+
+std::vector<double> Force::getValue() const
+{
+	return value;
 }
