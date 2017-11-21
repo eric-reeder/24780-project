81b40d6ce577af755c5d87f1f193cea1b81b9602
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-11-21 08:08:43 -0500

    Add files via upload

--- PlotWindow.cpp
+++ PlotWindow.cpp
@@ -1,101 +1,16 @@
 #include <stdio.h>
-#include <GraphingFunction.h>
+#include <PlotWindow.h>
 #include "fssimplewindow.h"
 
-PositionData::PositionData()
-{//This is the constructor for the position data. 
-	position = NULL;
-	CleanUp();
-}
-
-PositionData::~PositionData()
-{//This is the destructor. 
-	CleanUp();
-}
-
-void PositionData::CleanUp(void)
-{//This deletes the position data
-	if (NULL != data)
-	{
-		delete[] dat;
-	}
-	dat = NULL;
-	timestep = 0; //initializes the time step 
-}
-
-PositionData::PositionData(const PositionData &incoming)
-{
-	dat = nullptr;
-	CopyFrom(incoming);
-}
-
-void PositionData::CopyFrom(const PositionData &incoming)
-{//Copy Constructor 
-	if (&incoming != this)
-	{
-		CleanUp();
-		dat = new char[incoming.timestep];
-		for (int i = 0; i<incoming.timestep; ++i)
-		{
-			dat[i] = incoming.dat[i];
-		}
-		timestep = incoming.timestep;
-	}
-}
-
-void PositionData::GraphPosition(void)
-{//This grapsh the position data 
+void GraphPosition::GraphPosition(void)
+{//This graphs the position data 
 	double timestep;
 	glColor3ub(0, 0, 0);
 	glBegin(GL_LINE_STRIP);
 	const double y = timestep*x;
 }
-
-//This repeats for the Velocity Data
-VelocityData::VelocityData()
-{//This is the constructor for the position data. 
-	velocity = NULL;
-	CleanUp();
-}
-
-VelocityData::~VelocityData()
-{//This is the destructor. 
-	CleanUp();
-}
-
-void VelocityData::CleanUp(void)
-{//This deletes the position data
-	if (NULL != data)
-	{
-		delete[] dat;
-	}
-dat = NULL;
-timestep = 0; //initializes the time step 
-}
-
-VelocityData::VelocityData(const VelocityData &incoming)
-{
-	dat = nullptr;
-	CopyFrom(incoming);
-}
-
-void VelocityData::CopyFrom(const VelocityData &incoming)
-{//Copy Constructor 
-	if (&incoming != this)
-	{
-		CleanUp();
-		dat = new char[incoming.timestep];
-		for (int i = 0; i<incoming.timestep; ++i)
-		{
-			dat[i] = incoming.dat[i];
-		}
-		timestep = incoming.timestep;
-	}
-}
-
-
 void VelocityData::GraphVelocity(void)
-{
+{//This graphs the velocity data
 	double timestep;
 	glColor3ub(0, 0, 0);
 	glBegin(GL_LINE_STRIP);
--- PlotWindow.h
+++ PlotWindow.h
@@ -1,39 +1,23 @@
 #ifndef _PlotWindow_IS_INCLUDED_ 
 #define _PlotWindow_IS_INCLUDED_
 
-class PositionData
-{ //This class creates the position data as a variable array 
-protected: 
-	double timestep; //timestep variable 
-	double *position[]; //creates empty array for position data 
+
+class GraphPosition
+{
+protected:
+	double position; 
 
 public: 
-	PositionData(); //constructor
-	~PositionData(); //destructor 
-	void CleanUp(); //cleans up data 
-
-	PositionData(const PositionData &incoming); //copy constructor 
-	void CopyFrom(const PositionData &incoming);
-
-	void GraphPosition(void); //graphs data
+	void GraphPosition(void);
 };
 
-class VelocityData
-{//This class creates the velocity data as an incoming array
+class GraphVelocity
+{
 protected: 
-	double timestep;  //timestep variable 
-	double *velocity[]; //creates empty array for velocity 
+	double velocity; 
 
 public: 
-	VelocityData(); //constructor
-	~VelocityData(); //deconstructor 
-	void CleanUp(); //cleans up data
-
-	VelocityData(const VelocityData &incoming); //copy constructor 
-	void CopyFrom(const VelocityData &incoming);
-
-	void GraphVelocity(void); //graphs data 
-
+	void GraphVelocity(void);
 };
 
 #endif
