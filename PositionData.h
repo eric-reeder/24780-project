b00c923b59a6b44475fc7266fde25f5edee6e07e
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-11-21 07:26:33 -0500

    Add files via upload
    Added PositionData header for position array function

---
+++ PositionData.h
@@ -0,0 +1,21 @@
+#ifndef _PositionData_IS_INCLUDED_
+#define _PositionData_IS_INCLUDED_
+
+class PositionData
+{ //This class creates the position data as a variable array 
+protected:
+	double timestep; //timestep variable 
+	double *position[]; //creates empty array for position data 
+
+public:
+	PositionData(); //constructor
+	~PositionData(); //destructor 
+	void CleanUp(); //cleans up data 
+
+	PositionData(const PositionData &incoming); //copy constructor 
+	void CopyFrom(const PositionData &incoming);
+
+	void GraphPosition(void); //graphs data
+};
+
+#endif
