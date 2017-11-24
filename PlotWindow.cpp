e0d939ce5d0b9c339064ca40069b95a8b90cf0ff
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-11-23 23:17:18 -0500

    Updated classes with GraphicsWindow inheritance

--- PlotWindow.cpp
+++ PlotWindow.cpp
@@ -1,18 +1,91 @@
 #include <stdio.h>
 #include <PlotWindow.h>
-#include "fssimplewindow.h"
 
-void GraphPosition::GraphPosition(void)
-{//This graphs the position data 
-	double timestep;
-	glColor3ub(0, 0, 0);
-	glBegin(GL_LINE_STRIP);
-	const double y = timestep*x;
+//Eric, this is copied from your GraphicsWindow class and assumed we could just determine
+//the approximate coordinates. I put in dummy values for now. 
+
+
+//Set the x- and y-positions to 0. (Eric, I wasn't sure about the exact coordinates.) 
+//This may need to be xPlotPosition or something similar relative to the position of the Animation Window. 
+const int xPosition = 0; 
+const int yPosition = 0; 
+
+/*Creates a graphics window object with black border and white background
+Inputs :
+appWidth: Width of app window
+	appHeight : Height of app window
+	inputMargin : Margin between this window and other windows
+	widthFraction : Fraction of app window width this window will be
+	heightFraction : Fraction of app window height this window will be
+	inputXPosition : X coordinate of top left corner
+	inputYPosition : Y coordinate of top left corner  * /
+	
+
+/*  Sets x and y coordinates of top left corner
+		  Inputs:
+		  newXPosition: X coordinate of top left corner
+		  newYPosition: Y coordinate of top left corner  */
+
+	GraphicsWindow::GraphicsWindow()
+{
+	borderWeight = 2;
+	borderRed = 0;
+	borderGreen = 0;
+	borderBlue = 0;
+
+	backgroundRed = 255;
+	backgroundGreen = 255;
+	backgroundBlue = 255;
 }
-void VelocityData::GraphVelocity(void)
-{//This graphs the velocity data
-	double timestep;
-	glColor3ub(0, 0, 0);
-	glBegin(GL_LINE_STRIP);
-	const double y = timestep*x;
+
+void GraphicsWindow::setPosition(const int newXPosition, const int newYPosition)
+{
+	xPosition = newXPosition;
+	yPosition = newYPosition;
+}
+
+void GraphicsWindow::setSize(const int newWidth, const int newHeight)
+{
+	width = newWidth;
+	height = newHeight;
+}
+
+/*  Draws background of graphics window  */
+void GraphicsWindow::drawBackground(void) const
+{
+	glColor3ub(backgroundRed, backgroundGreen, backgroundBlue);
+	glBegin(GL_QUADS);
+	glVertex2i(xPosition, yPosition);
+	glVertex2i(xPosition + width, yPosition);
+	glVertex2i(xPosition + width, yPosition + height);
+	glVertex2i(xPosition, yPosition + height);
+	glEnd();
+}
+
+/*  Draws border on graphics window  */
+void GraphicsWindow::drawBorder(void) const
+{
+	glColor3ub(borderRed, borderGreen, borderBlue);
+	glBegin(GL_LINES);
+	glLineWidth(borderWeight);
+
+	glVertex2i(xPosition, yPosition);
+	glVertex2i(xPosition + width, yPosition);
+
+	glVertex2i(xPosition + width, yPosition);
+	glVertex2i(xPosition + width, yPosition + height);
+
+	glVertex2i(xPosition + width, yPosition + height);
+	glVertex2i(xPosition, yPosition + height);
+
+	glVertex2i(xPosition, yPosition + height);
+	glVertex2i(xPosition, yPosition);
+	glEnd();
+}
+
+/*  Draws the graphics window  */
+void GraphicsWindow::draw() const
+{
+	drawBackground();
+	drawBorder();
 }
