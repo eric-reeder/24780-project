8524161b7e051b9dc21e6818ee1103b5d7e88ae6
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-11-23 23:11:11 -0500

    Edited class inheritance

--- PlotWindow.h
+++ PlotWindow.h
@@ -5,11 +5,8 @@
 
 //This class creates the plot window. It inherits information from the Graphics Class. 
 
-class PlotWindow::public GraphicsWindow
+class PlotWindow:public GraphicsWindow
 {//Public memebers from the GraphicsWindow class will be available. 
-
-public: 
-	void reset(void);
 };
 
 #endif
