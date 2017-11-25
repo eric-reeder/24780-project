2014d34f82d9c89957d0ff79df75bad75b6222f4
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-11-25 15:54:44 -0600

    Fixed inheritance issues and added functions
    Edited header to inherit from GraphicsWindow 
    Created displacement and velocity informtion using std::vector (and deleted functions for creating variable-length arrays manually) 
    Added graphing functions

--- PlotWindow.h
+++ PlotWindow.h
@@ -2,11 +2,32 @@
 #define _PLOTWINDOW_IS_INCLUDED_ 
 
 #include "GraphicsWindow.h"
+#include "DynamicSystemApp.h"
+#include <vector>
 
 //This class creates the plot window. It inherits information from the Graphics Class. 
 
-class PlotWindow:public GraphicsWindow
-{//Public memebers from the GraphicsWindow class will be available. 
+class PlotWindow : public GraphicsWindow
+{//Public members from the GraphicsWindow class will be available. 
+
+protected: 
+	std::vector<double> disp, velocity; //creates variable arrays for the displacement and velocity
+	double timeStep; //Time step variable
+	double time; //Time variable
+
+public:
+	PlotWindow(); //Constructor 
+	
+	void reset(void); //function to reset window
+	void DrawAxes(void); //function to draw axes 
+	void GraphPosition(void); //function for drawing position graph
+	void LabelPosition(void); //labels position graph
+	void GraphVelocity(void); //function for 
+	void LabelVelocity(void);
+	CleanUp();
+
+	~PlotWindow(); //deconstructor 
+
 };
 
 #endif
