6786eb4fe3ebcea5035387350268cd29f7fabf09
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-11-26 10:31:54 -0500

    Update PlotWindow.h
    Added additional graphics information and removed extraneous functions

--- PlotWindow.h
+++ PlotWindow.h
@@ -3,7 +3,7 @@
 
 #include "GraphicsWindow.h"
 #include "DynamicSystemApp.h"
-#include <vector>
+#include<vector>
 
 //This class creates the plot window. It inherits information from the Graphics Class. 
 
@@ -15,19 +15,45 @@ protected:
 	double timeStep; //Time step variable
 	double time; //Time variable
 
-public:
-	PlotWindow(); //Constructor 
+	int plotWeight;
+				 
+	//defines width of plot window
+	int PlotWindowWidth; 
+	int PlotWindowHeight; 
+
+	//sets origin of sub window
+	int PlotWindowxPosition;
+	int PlotWindowyPosition;
 	
-	void reset(void); //function to reset window
-	void DrawAxes(void); //function to draw axes 
+	//defines location of position graph 
+	int PositionXAxisLength;
+	int PositionOrigin
+	int PositionYAxisHeight;
+
+	//defines location of velocity graph
+	int VelocityXAxisLength;
+	int PositionOrigin
+	int VelocityYAxisHeight;
+
+	//Margin between border window and position graphs
+	int PositionGraphXMargin; 
+	int PositionGraphYMargin; 
+
+	//Margin between boder window and velocity graphs 
+	int VelocityGraphXMargin; 
+	int VelocityGraphYMargin; 
+	
+public:
+	PlotWindow(); //Initalizes plot window
+
+	void DrawPositionAxes(void); //function to draw axes 
+	void DrawVelocityAxes(void);
 	void GraphPosition(void); //function for drawing position graph
 	void LabelPosition(void); //labels position graph
-	void GraphVelocity(void); //function for 
+	void GraphVelocity(void); //function for graphing velocity data
 	void LabelVelocity(void);
-	CleanUp();
 
-	~PlotWindow(); //deconstructor 
-
+	CleanUp(); //frees up memory for displacement and velocity vectors 
 };
 
 #endif
