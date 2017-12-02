fd432ddb3318d1f742e8bf1e41d390ee47e6443b
stephaniejennings <33583324+stephaniejennings@users.noreply.github.com>
2017-12-02 18:56:39 -0500

    commented out some functions for debugging

--- PlotWindow.h
+++ PlotWindow.h
@@ -12,8 +12,8 @@ class PlotWindow : public GraphicsWindow
 
 protected:
 	
-	int axislength;
-	int axisheight;
+	/*int axislength;
+	int axisheight;*/
 
 	int axiscolorRed;
 	int axiscolorGreen;
@@ -27,19 +27,26 @@ protected:
 	int plot2Green; 
 	int plot2Blue; 
 
-	int plotx1; 
+	/*int plotx1; 
 
 	double position1;
 	double position2;
-
+*/
 	double velocity1;
 	double velocity2;
 
-	double xstart; 
-	double ystart; 
+	//double xstart; 
+	//double ystart;*/ 
+
+	double magnifier;
+
+	double positiontranslation; 
+	double velocitytranslation; 
 
 	std::vector <double> storedvelocity1;
 	std::vector <double> storedvelocity2;
+
+	double plotvelocity1[100];
 
 	std::vector <double> storedposition1;
 	std::vector <double> storedposition2;
@@ -54,14 +61,14 @@ public:
 	void DrawPositionAxes(void);
 	void DrawVelocityAxes(void); 
 
-	//function to draw graphs 
-	void GraphPosition(void); //function for drawing position graph
+	////function to draw graphs 
+	//void GraphPosition(void); //function for drawing position graph
 	void GraphVelocity(void);
-	
-	
-	//Functions that call real-time positions and velocities from the Mass class
+	//
+	//
+	////Functions that call real-time positions and velocities from the Mass class
 	void Velocity(Mass mass1, Mass mass2);
-	void Position(Mass mass1, Mass mass2);
+	//void Position(Mass mass1, Mass mass2);
 
 	//Draws Windows
 	void draw(Mass mass1, Mass mass2, double timeStep);
