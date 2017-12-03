00d52fe1488e80e20b8ad1722a086359f99b9bd8
SagarShetye <33704428+SagarShetye@users.noreply.github.com>
2017-12-03 02:58:10 -0500

    Modified GraphVelocity() and GraphPosition()

--- PlotWindow.h
+++ PlotWindow.h
@@ -11,40 +11,44 @@ class PlotWindow : public GraphicsWindow
 {//Public members from the GraphicsWindow class will be available. 
 
 protected:
-	
+
 	/*int axislength;
 	int axisheight;*/
 
 	int axiscolorRed;
 	int axiscolorGreen;
 	int axiscolorBlue;
-	
-	int plot1Red; 
-	int plot1Green; 
+
+	int plot1Red;
+	int plot1Green;
 	int plot1Blue;
 
-	int plot2Red; 
-	int plot2Green; 
-	int plot2Blue; 
+	int plot2Red;
+	int plot2Green;
+	int plot2Blue;
 
-	/*int plotx1; 
+	/*int plotx1;
 
 	double position1;
 	double position2;
-*/
+	*/
 	double velocity1;
 	double velocity2;
 
 	//double xstart; 
 	//double ystart;*/ 
 
-	double magnifier;
+	// Magnifiers for position and velocity
+	double magnifier_pos;
+	double magnifier_vel;
 
-	double positiontranslation; 
-	double velocitytranslation; 
+	double positiontranslation;
+	double velocitytranslation;
 
 	std::vector <double> storedvelocity1;
 	std::vector <double> storedvelocity2;
+
+	std::vector<double> timePeriods;				// Stores all the different time values at which calculations are done
 
 	double plotvelocity1[100];
 
@@ -54,25 +58,27 @@ protected:
 public:
 	PlotWindow(); //Initalizes plot window
 
-	//Retrieves coordinates from Animation Class
-	//void GetCoordinates(void);
-				  
-	//function to draw axes 
+				  //Retrieves coordinates from Animation Class
+				  //void GetCoordinates(void);
+
+				  //function to draw axes 
 	void DrawPositionAxes(void);
-	void DrawVelocityAxes(void); 
+	void DrawVelocityAxes(void);
 
 	////function to draw graphs 
-	//void GraphPosition(void); //function for drawing position graph
-	void GraphVelocity(void);
+	void GraphPosition(double pos1, double pos2, double time, double maxTime); //function for drawing position graph
+	void GraphVelocity(double vel1, double vel2, double time, double maxTime);
 	//
 	//
 	////Functions that call real-time positions and velocities from the Mass class
-	void Velocity(Mass mass1, Mass mass2);
-	//void Position(Mass mass1, Mass mass2);
+	void Velocity(double vel1, double vel2, double time);
+	void Position(double pos1, double pos2);
 
 	//Draws Windows
-	void draw(Mass mass1, Mass mass2, double timeStep);
-	
+	//void draw(double vel1, double vel2, double time, double maxTime);
+
+	void plot(double vel1, double vel2, double pos1, double pos2, double time, double maxTime);
+
 	//Resets info 
 	void reset(void);
 };
